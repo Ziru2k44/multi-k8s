0d47e0e55cfc6f2f6550922d91b5a314218dd607
Ziru2k44 <bernardo.b.lourenco@gmail.com>
2019-08-05 22:21:40 +0100

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 docker build -t bernardobraslourenco/multi-client:latest -t bernardobraslourenco/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t bernardobraslourenco/multi-server:latest -t bernardobraslourenco/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t bernardobraslourenco/multi-worker:latest -t bernardobraslourenco/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push bernardobraslourenco/muilt-client:latest
-docker push bernardobraslourenco/muilt-client:$SHA
-docker push bernardobraslourenco/muilt-server:latest
-docker push bernardobraslourenco/muilt-server:$SHA
-docker push bernardobraslourenco/muilt-worker:latest
-docker push bernardobraslourenco/muilt-worker:$SHA
+docker push bernardobraslourenco/multi-client:latest
+docker push bernardobraslourenco/multi-client:$SHA
+docker push bernardobraslourenco/multi-server:latest
+docker push bernardobraslourenco/multi-server:$SHA
+docker push bernardobraslourenco/multi-worker:latest
+docker push bernardobraslourenco/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=bernardobraslourenco/muilt-server:$SHA
-kubectl set image deployments/client-deployment client=bernardobraslourenco/muilt-client:$SHA
-kubectl set image deployments/worker-deployment worker=bernardobraslourenco/muilt-worker:$SHA
+kubectl set image deployments/server-deployment server=bernardobraslourenco/multi-server:$SHA
+kubectl set image deployments/client-deployment client=bernardobraslourenco/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=bernardobraslourenco/multi-worker:$SHA
